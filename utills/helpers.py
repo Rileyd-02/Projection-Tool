ba60f25d5c78bee6aa9c41e8805d6aa8762f614c
Riley Douglas <129730881+Rileyd-02@users.noreply.github.com>
2025-10-29 13:36:41 +0530

    Update helpers.py

--- utills/helpers.py
+++ utills/helpers.py
@@ -1,8 +1,8 @@
+# utils/helpers.py
 import pandas as pd
 from io import BytesIO
 
 def excel_to_bytes(df: pd.DataFrame, sheet_name: str = "Sheet1"):
-    """Return bytes buffer of an Excel file for download."""
     output = BytesIO()
     with pd.ExcelWriter(output, engine="openpyxl") as writer:
         df.to_excel(writer, index=False, sheet_name=sheet_name)
